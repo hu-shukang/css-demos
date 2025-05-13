4fadad333b217ba38d1b6be77cd8f8eacdb1b528
HU SHUKANG <hushukang@gmail.com>
2025-05-13 11:51:00 +0900

    Update verify-auth-challenge-response.ts

--- verify-auth-challenge-response.ts
+++ verify-auth-challenge-response.ts
@@ -1,7 +1,9 @@
-import { CognitoUserPoolTriggerEvent, CognitoUserPoolTriggerHandler } from 'aws-lambda';
+import {
+  CognitoUserPoolTriggerEvent,
+  CognitoUserPoolTriggerHandler,
+} from 'aws-lambda';
 
 export const handler: CognitoUserPoolTriggerHandler = async (event) => {
-  // 仅在验证阶段执行
   if (event.triggerSource !== 'VerifyAuthChallengeResponse_Authentication') {
     return event;
   }
@@ -9,13 +11,10 @@ export const handler: CognitoUserPoolTriggerHandler = async (event) => {
   if (event.request.challengeName === 'CUSTOM_CHALLENGE') {
     const answer = event.request.challengeAnswer || '';
     const username = event.userName;
-
-    // 检查新密码是否包含用户名（不区分大小写）
-    const containsUsername = answer
+    // 密码里不能包含用户名
+    event.response.answerCorrect = !answer
       .toLowerCase()
       .includes(username.toLowerCase());
-
-    event.response.answerCorrect = !containsUsername;
   }
 
   return event;
